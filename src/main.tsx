9ad13c328f2c285e5440ee69e597770f7ef3aef6
orafamoura <raphaelmvb2@gmail.com>
2025-08-31 20:11:54 -0300

    feat: criado texto infinito(incompleto) e configurações no css para funcionar

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/Home'
-import './pages/Home/App.css'
+import './app.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
