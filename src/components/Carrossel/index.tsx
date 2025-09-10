4f93e02c5e85226528a0d6106a30d3dfdf3157fb
orafamoura <raphaelmvb2@gmail.com>
2025-09-10 00:43:36 -0300

    refactor: swiper zerado

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -4,33 +4,53 @@ import CarrosselItem from "./items";
 import 'swiper/swiper-bundle.css';
 import 'swiper/modules'
 
-
 export default function Carrossel() {
+
+    const itemsDoCarrossel = [
+        {"carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
+        "e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e " +
+        "promovem bem-estar e qualidade de vida ao longo do envelhecimento.",
+        "carrosselTitulo":"Manter o Cérebro Ativo",
+        "carrosselImg":"carrosselImagens/exemplo.png"
+        },
+        {"carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
+        "e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e " +
+        "promovem bem-estar e qualidade de vida ao longo do envelhecimento.",
+        "carrosselTitulo":"Manter o Cérebro ",
+        "carrosselImg":"carrosselImagens/exemplo.png"
+        },
+        {"carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
+        "e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e " +
+        "promovem bem-estar e qualidade de vida ao longo do envelhecimento.",
+        "carrosselTitulo":"Manter o  Ativo",
+        "carrosselImg":"carrosselImagens/exemplo.png"
+        }
+    ]
+
     return (
         <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCards]}
-        spaceBetween={25}
+        spaceBetween={30}
         initialSlide={1}
-        slidesPerView='auto'
+        centeredSlides={true}
+        slidesPerView={1}
         autoplay={{delay: 3000}}
         breakpoints={{
-            640:  { slidesPerView: 1.3, autoplay:{delay: 3000 }},
-            1024: { slidesPerView: 1.5, autoplay:{delay: 3000 }},
+            640:  { slidesPerView: 1.8, autoplay:{delay: 3000 }},
+            1024: { slidesPerView: 3, autoplay:{delay: 3000 }},
             1280: { initialSlide: 1, slidesPerView: 3, autoplay: false, enabled: false},
         }}
-        centeredSlides={true}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
-            className="w-full sm:max-w-3xl md:max-w-lg lg:max-w-3xl xl:max-w-6xl px-4!">
-            <SwiperSlide >
-                <CarrosselItem carrosselTexto="Mantenha seu cérebro ativo e sua autonomia preservada com atividades e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e promovem bem-estar e qualidade de vida ao longo do envelhecimento." carrosselTitulo="Manter o Cérebro Ativo" carrosselImg="../../../public/carrosselImagens/exemplo.png"></CarrosselItem>
-            </SwiperSlide>
-            <SwiperSlide >
-                <CarrosselItem carrosselTexto="Mantenha seu cérebro ativo e sua autonomia preservada com atividades e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e promovem bem-estar e qualidade de vida ao longo do envelhecimento." carrosselTitulo="Manter o Cérebro Ativo" carrosselImg="../../../public/carrosselImagens/exemplo.png"></CarrosselItem>
-            </SwiperSlide>
-            <SwiperSlide >
-                <CarrosselItem carrosselTexto="Mantenha seu cérebro ativo e sua autonomia preservada com atividades e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e promovem bem-estar e qualidade de vida ao longo do envelhecimento." carrosselTitulo="Manter o Cérebro Ativo" carrosselImg="../../../public/carrosselImagens/exemplo.png"></CarrosselItem>
-            </SwiperSlide>
+        className="w-full">
+            {itemsDoCarrossel.map(function (item) {
+                return (
+                    <SwiperSlide className=''>
+                        <CarrosselItem carrosselImg={item.carrosselImg} carrosselTexto={item.carrosselTexto} carrosselTitulo={item.carrosselTitulo}></CarrosselItem>
+                    </SwiperSlide>
+                )
+            })}
+            
         </Swiper>
     )
 }
