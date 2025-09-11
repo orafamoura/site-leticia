32faf5028fd4e3c0a030753fd753b3457f69d745
orafamoura <raphaelmvb2@gmail.com>
2025-09-11 00:46:30 -0300

    refactor: trocando swiperjs por react e tailwind

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -1,5 +1,3 @@
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCards } from 'swiper/modules';
-import { Swiper, SwiperSlide } from "swiper/react";
 import CarrosselItem from "./items";
 import 'swiper/swiper-bundle.css';
 import 'swiper/modules'
@@ -7,19 +5,25 @@ import 'swiper/modules'
 export default function Carrossel() {
 
     const itemsDoCarrossel = [
-        {"carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
+        {
+        "id": 1,
+        "carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
         "e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e " +
         "promovem bem-estar e qualidade de vida ao longo do envelhecimento.",
         "carrosselTitulo":"Manter o Cérebro Ativo",
         "carrosselImg":"carrosselImagens/exemplo.png"
         },
-        {"carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
+        {
+        "id": 2, 
+        "carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
         "e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e " +
         "promovem bem-estar e qualidade de vida ao longo do envelhecimento.",
         "carrosselTitulo":"Manter o Cérebro ",
         "carrosselImg":"carrosselImagens/exemplo.png"
         },
-        {"carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
+        {
+        "id": 3,    
+        "carrosselTexto": "Mantenha seu cérebro ativo e sua autonomia preservada com atividades" + 
         "e exercícios personalizados que estimulam a mente, previnem perdas cognitivas e " +
         "promovem bem-estar e qualidade de vida ao longo do envelhecimento.",
         "carrosselTitulo":"Manter o  Ativo",
@@ -28,29 +32,18 @@ export default function Carrossel() {
     ]
 
     return (
-        <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCards]}
-        spaceBetween={30}
-        initialSlide={1}
-        centeredSlides={true}
-        slidesPerView={1}
-        autoplay={{delay: 3000}}
-        breakpoints={{
-            640:  { slidesPerView: 1.8, autoplay:{delay: 3000 }},
-            1024: { slidesPerView: 3, autoplay:{delay: 3000 }},
-            1280: { initialSlide: 1, slidesPerView: 3, autoplay: false, enabled: false},
-        }}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-        className="w-full">
-            {itemsDoCarrossel.map(function (item) {
-                return (
-                    <SwiperSlide className=''>
-                        <CarrosselItem carrosselImg={item.carrosselImg} carrosselTexto={item.carrosselTexto} carrosselTitulo={item.carrosselTitulo}></CarrosselItem>
-                    </SwiperSlide>
-                )
-            })}
-            
-        </Swiper>
+       <section>
+            <div>
+                <div className='w-full flex gap-2'>
+                    {itemsDoCarrossel.map(function(item) {
+                        return (
+                            <div key={item.id} className=''>
+                                <CarrosselItem  carrosselImg={item.carrosselImg} carrosselTexto={item.carrosselTexto} carrosselTitulo={item.carrosselTitulo}></CarrosselItem>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+       </section>
     )
 }
--- src/components/Carrossel/items.tsx
+++ src/components/Carrossel/items.tsx
@@ -9,15 +9,16 @@ interface carrosselItemProps {
 
 export default function CarrosselItem({carrosselTitulo, carrosselTexto, carrosselImg}: carrosselItemProps){
     return (
-        <div className="w-full h-auto max-w-sm bg-azul-base shadow-sm rounded-t-lg">
-            <img className="w-full h-auto max-h-[150px] block  rounded-t-lg" src={carrosselImg} alt="" />
-            <div className="p-5">
+        <div className="max-w-[380px] min-w-[340px]">
+            <img className="w-full h-[140px] rounded-t-lg" src={carrosselImg} alt="" />
+            <div className="border-transparent bg-azul-base">
                 <a>
-                    <h5 className="text-xs sm:text-sm md:text-md lg:text-xl leading-5 sm:leading-8 mb-2 font-bold tracking-tight text-white">{carrosselTitulo}</h5>
+                    <h5 className="">{carrosselTitulo}</h5>
                 </a>
-                <p className="mb-3 text-xs sm:text-sm md:text-md lg:text-xl text-white">{carrosselTexto}</p>
+                <p className="">{carrosselTexto}</p>
                 <Button>Saiba Mais</Button>
             </div>
         </div>
+
     )
 }
