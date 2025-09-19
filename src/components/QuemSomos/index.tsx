f836e94bde5dbe64ef84f6ba7382216fffff9edc
orafamoura <raphaelmvb2@gmail.com>
2025-09-19 19:48:30 -0300

    refactor: section quemSomos refatorada, incompleta ainda

--- src/components/QuemSomos/index.tsx
+++ src/components/QuemSomos/index.tsx
@@ -1,42 +1,40 @@
-import ItemsQuemSomos from "./items";
+import Button from "../Button";
 
 export default function QuemSomos() {
 
-    const quemSomosItems = [
-        {
-            "quemSomosCss":"order-2 rounded-r-[100px] border-l-2",
-            "quemSomosImg":"order-2 bg-[url('/images/sociaLeticia.webp')]",        
-            "quemSomosTitulo":"Leticia Dantas",
-            "quemSomosTexto":"Cofundadora do Instituto Inclusio, Terapeuta Ocupacional especializada em "+
-                " Reabilitação Neurológica. Atua com foco em autonomia, qualidade de vida e cuidado humanizado.",
-            "quemSomosItem1":"Especialista em Neuro",
-            "quemSomosItem2":"Reabilitação Neurológica",
-        },
-        {
-            "quemSomosCss":"order-1 rounded-l-[100px] border-r-2",
-            "quemSomosImg":"bg-[url('/images/sociaAngela.webp')]",        
-            "quemSomosTitulo":"Angela Dantas",
-            "quemSomosTexto":"Cofundadora do Instituto Inclusio, Terapeuta Ocupacional especializada em "+
-                " Reabilitação Neurológica. Atua com foco em autonomia, qualidade de vida e cuidado humanizado.",
-            "quemSomosItem1":"Especialista em Neuro",
-            "quemSomosItem2":"Reabilitação Neurológica",
-        },
-    ]
     return(
-        <div className="flex flex-row justify-evenly items-center my-25">
-            <p className="order-2 xl:text-7xl 2xl:text-8xl max-w-[400px] font-bold leading-4.5 xl:leading-15 2xl:leading-20 text-center ">QUEM SOMOS</p>
-            {quemSomosItems.map(function(item){
-                return (
-                    <ItemsQuemSomos
-                        quemSomosCss={item.quemSomosCss} 
-                        quemSomosImg={item.quemSomosImg} 
-                        quemSomosTitulo={item.quemSomosTitulo}
-                        quemSomosTexto={item.quemSomosTexto}
-                        quemSomosItem1={item.quemSomosItem1}
-                        quemSomosItem2={item.quemSomosItem2}>
-                    </ItemsQuemSomos>
-                )
-            })}
+        <div className="w-full flex flex-col lg:grid lg:grid-cols-4 xl:flex xl:flex-row gap-5 justify-center items-center my-25 lg:px-6">
+            <p className="order-1 lg:col-start-2 lg:col-span-2 xl:order-2 text-5xl xl:text-8xl max-w-[400px] font-bold leading-4.5 lg:leading-10 xl:leading-15 2xl:leading-20 text-center ">QUEM SOMOS</p>
+            <div className="w-full min-h-[300px] md:min-h-100 max-w-[450px] lg:col-start-0 lg:col-span-2 flex flex-row border-2 overflow-hidden order-3 self-start sm:self-center ml-0 lg:ml-0 sm:ml-40 rounded-r-[100px] border-l-2">
+                <div className="w-full min-w-[175px] bg-cover bg-top block order-2 bg-[url('/images/sociaLeticia.webp')]"></div>
+                <div className="flex flex-col items-center self-center">
+                    <div className="flex flex-col text-right m-4">
+                        <h4 className="text-dourado-base">Leticia Dantas</h4>
+                        <p className="text-white text-sm">Cofundadora do Instituto Inclusio, Terapeuta Ocupacional especializada em Reabilitação Neurológica. 
+                            Atua com foco em autonomia, qualidade de vida e cuidado humanizado.</p>
+                        <div className="flex flex-col">
+                            <p>Especialista em Neuro</p>
+                            <p>Reabilitação Neurológica</p>
+                        </div>
+                    </div>
+                    <Button className="bg-botao-wpp hover:bg-[#099656] text-white">AGENDAR</Button>
+                </div>
+            </div>             
+            <div className="min-h-[300px] md:min-h-100 max-w-[450px] lg:col-start-1 lg:col-end-3 flex flex-row border-2 overflow-hidden order-1 self-end sm:self-center mr-0 lg:mr-0 sm:mr-40 rounded-l-[100px] border-r-2">
+                <div className="w-full min-w-[175px] bg-cover bg-top block bg-[url('/images/sociaAngela.webp')]"></div>
+                <div className="flex flex-col items-center self-center">
+                    <div className="flex flex-col text-left m-4">
+                        <h4 className="text-dourado-base">Angela Dantas</h4>
+                        <p className="text-white text-sm">Cofundadora do Instituto Inclusio, Terapeuta Ocupacional especializada em Reabilitação Neurológica. 
+                            Atua com foco em autonomia, qualidade de vida e cuidado humanizado.</p>
+                        <div className="flex flex-col">
+                            <p>Especialista em Neuro</p>
+                            <p>Reabilitação Neurológica</p>
+                        </div>
+                    </div>
+                    <Button className="bg-botao-wpp hover:bg-[#099656] text-white">AGENDAR</Button>
+                </div>
+            </div>             
         </div>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -57,7 +57,7 @@ export default function Home(){
                         <Button className="bg-dourado-base text-white hover:bg-[#bb8e47]">QUERO SABER MAIS</Button>
                     </div>
                 </section>
-                <section className="w-full px-6 bg-azul-base">
+                <section className="w-full bg-azul-base">
                     <QuemSomos></QuemSomos>
                 </section>
             </div>
