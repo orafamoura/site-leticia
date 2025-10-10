1abbb8d876362b76a317f9486b523bdf81dacee2
orafamoura <raphaelmvb2@gmail.com>
2025-10-10 00:16:06 -0300

    style: estilizacao da section principal melhorada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,25 +11,36 @@ export default function Home(){
         <div className="font-montserrat">
             <Header></Header>
             <div className=" flex flex-col gap-6 max-w-[1920px] 3xl:justify-self-center">
-                <section className="w-full">
-                    <div className="w-auto relative">
-                        <img className="block w-full h-[350px] sm:h-[800px] object-cover object-center sm:object-top" src="/images/fotoPrincipal.webp" alt="foto" />
-                        <div className="absolute inset-0 bg-gradient-to-t from-degrade-cinza from-40% sm:from-20% via-degrade-cinza-transparente via-55% sm:via-40% to-transparent to-65%"></div>
-                        <div className="flex flex-col absolute inset-x-0 bottom-0 md:mb-30 gap-5 items-center mx-4 md:mx-0">
+                <section className="w-full h-lvh min-h-svh max-h-lvh">
+                    <div className="w-auto relative h-auto overflow-hidden">
+                        <img className="w-full sm:border-b-[0px] h-lvh max-h-[600px] object-cover object-top sm:object-top" src="/images/fotoPrincipal.webp" alt="foto" />
+                        <div className=" absolute inset-0 bottom-0 bg-gradient-to-t from-degrade-cinza from-30% sm:from-10% via-degrade-cinza-transparente via-45% sm:via-35% to-transparent to-55%"></div>
+                        <div className="flex flex-col absolute inset-x-0 bottom-10 gap-5 items-center mx-4 md:mx-0">
                             <div className="w-full h-auto flex flex-col md:flex-row justify-center items-center md:gap-9 gap-1">
-                                <img className="w-40 sm:w-40 md:w-45 lg:w-50 h-auto" src="/images/logoVertical.webp" alt=" Logo Vertical da empresa"/>
+                                <img className="w-40 sm:w-35 md:w-40 lg:w-45 h-auto" src="/images/logoVertical.webp" alt=" Logo Vertical da empresa"/>
                                 <a className="md:border-2 h-0 md:h-50 border-white"></a>
-                                <div className="w-auto max-w-170 flex flex-col gap-3 text-white items-center md:items-start text-center md:text-start">
+                                <div className="w-auto max-w-170 flex flex-col gap-3 text-white items-center md:items-start text-center md:text-start ">
                                     <h1 className="md:max-w-90 max-w-100 whitespace-normal text-cards leading-4.5 sm:leading-5 md:leading-5 font-bold">TERAPIA OCUPACIONAL E REABILITAÇÃO NEUROCOGNITIVA</h1>
                                     <p className="md:max-w-130 max-w-100 text-medio leading-4.5 sm:leading-5 md:leading-8 font-bold text-dourado-base md:mb-4"> RECUPERE A QUALIDADE DE VIDA E AUTONOMIA</p>
                                     <p className="md:max-w-none max-w-100 text-xs sm:text-base lg:text-lg border rounded-4xl text-center border-dourado-base px-2">Especializada para Adultos e Idosos em Brasilia</p>
                                 </div>
                             </div>
-                            <Button className="bg-botao-wpp hover:bg-[#099656] text-white">AGENDAR AVALIACAO</Button>
+                            <Button className="bg-botao-wpp hover:bg-[#099656] text-white">AGENDAR AVALIACAO</Button>                            
                         </div>
-                        <div className="w-full h-50 sm:h-20 bg-degrade-cinza"></div>
+                        <div className="absolute bottom-0 inset-x-0 left-0 right-0 w-full overflow-hidden leading-none">
+                            <svg
+                                className="relative block w-full h-7 md:h-7"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 1200 120"
+                                preserveAspectRatio="none"
+                            >
+                            <path
+                                d="M0,0 L600,120 L1200,0 L1200,120 L0,120 Z"
+                                fill="#ffffff"
+                            />
+                            </svg>
                     </div>
-                    <div className="w-full h-[30px] sm:h-[80px] bg-degrade-cinza [clip-path:polygon(0_0,100%_0,60%_80%,50%_100%,0_0%)]"></div>
+                    </div>
                 </section>
                 <section className="w-full h-auto flex flex-col gap-5 items-center ">
                     <h2 className="md:max-w-200 max-w-100 text-medio leading-4.5 sm:leading-5 md:leading-8 font-bold text-azul-base md:mb-4 text-center">PARA QUEM É A TERAPIA OCUPACIONAL E REABILITAÇÃO NEUROCOGNITIVA:</h2>
