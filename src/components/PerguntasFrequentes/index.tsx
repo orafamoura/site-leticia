0dc58d40a07b2b981847448fb320362059f4c6f0
orafamoura <raphaelmvb2@gmail.com>
2025-11-17 20:07:15 -0300

    feat: criado Items da section perguntasFrequentes

--- src/components/PerguntasFrequentes/index.tsx
+++ src/components/PerguntasFrequentes/index.tsx
@@ -1,4 +1,5 @@
 import Button from "../Button"
+import PerguntasFrequentesItem from "./items"
 
 export default function PerguntasFrequentes() {
 
@@ -90,16 +91,8 @@ export default function PerguntasFrequentes() {
                 <div className="flex flex-col">
                     {perguntas.map((item, key) => {
                         return (
-                            <details className="group border-2 rounded-3xl my-0.5 open:bg-azul-base open:text-white">
-                                    <summary className="flex min-h-[60px] mx-5 gap-7 items-center py-3 outline-none cursor-pointer focus:underline font-bold md:tracking-wider leading-[1] text-balance" key={key}>
-                                        <svg className="max-w-[15px] bg-center bg-no-repeat transition-transform duration-300 group-open:rotate-45 fill-azul-base group-open:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 717 715"><path d="M438 279h279v159H438v277H279V438H0V279h279V0h159v279z"/></svg>
-                                        {item.pergunta}
-                                    </summary>
-                                <div className="px-5 pb-4 flex-col text-balance">
-                                    <p className="" key={key}>{item.resposta}</p>
-                                </div>
-                            </details>
-                        )
+                            <PerguntasFrequentesItem pergunta={item.pergunta} resposta={item.resposta} key={key}></PerguntasFrequentesItem>
+                        )       
                     })}
                 </div>
             </div>     
