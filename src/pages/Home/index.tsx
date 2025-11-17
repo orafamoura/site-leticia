0a2d48aeac505ee3c9dbcbb4f2ce67b89e011f98
orafamoura <raphaelmvb2@gmail.com>
2025-11-17 19:05:29 -0300

    refactor: section principal mobile css

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,12 +21,19 @@ export default function Home(){
                         <div className="flex flex-col pb-10 absolute inset-x-0 bottom-10 gap-5 items-center mx-4 md:mx-0">
                             <div className="w-full h-auto flex flex-col md:flex-row justify-center items-center md:gap-9 gap-1">
                                 <div className="rounded-full bg-white p-4">
-                                    <img className="size-35 md:size-40 h-auto pb-3" src="/images/LogoVerticalColorida.svg" alt=" Logo Vertical da empresa"/>
+                                    <img className="size-30 md:size-40 h-auto pb-1 p-2 mb:pb-3 py-1" src="/images/LogoVerticalColorida.svg" alt=" Logo Vertical da empresa"/>
                                 </div>
-                                <div className="w-auto max-w-170 flex flex-col gap-1 text-white items-center md:items-start text-center md:text-start ">
-                                    <h1 className="md:max-w-2xl max-w-100 whitespace-normal text-normal leading-4.5 sm:leading-5 md:leading-5 font-bold">TERAPIA OCUPACIONAL E REABILITAÇÃO NEUROCOGNITIVA</h1>
-                                    <p className="md:max-w-2xl max-w-100 text-destaque leading-4.5 sm:leading-6 md:leading-7 font-extrabold text-dourado-base md:mb-2">ESTIMULE A SUA AUTONOMIA, INDEPENDÊNCIA E QUALIDADE DE VIDA</p>
-                                    <p className="md:max-w-none max-w-100 text-xs text-normal border rounded-4xl text-center border-dourado-base px-2">Especializada para Adultos e Idosos em Brasilia</p>
+                                <div className="w-auto flex flex-col gap-1 text-white items-center md:items-start text-center md:text-start ">
+                                    <h1 className="md:max-w-2xl max-w-100 whitespace-normal text-normal leading-4.5 sm:leading-5 md:leading-5 font-bold text-balance">TERAPIA OCUPACIONAL E REABILITAÇÃO NEUROCOGNITIVA</h1>
+                                        <div className="w-full md:max-w-2xl flex flex-col items-center md:items-start text-center md:text-left tracking-wide text-dourado-base font-extrabold">
+                                            <p className="text-lg leading-4.5 md:text-destaque sm:leading-6 md:leading-7">
+                                                ESTIMULE A SUA AUTONOMIA,
+                                            </p>
+                                            <p className="px-4 md:px-0 text-[1.6rem] md:text-destaque leading-[.9] md:mb-2">
+                                                INDEPENDÊNCIA E QUALIDADE DE VIDA
+                                            </p>
+                                        </div>
+                                    <p className="md:max-w-none max-w-80 px-2 py-1 my-3 text-normal text-center text-balance leading-[1] border rounded-4xl border-dourado-base ">Especializada para Adultos e Idosos em Brasilia</p>
                                 </div>
                             </div>
                             <Button className="bg-botao-wpp hover:bg-[#099656] text-white">AGENDAR AVALIACAO</Button>                            
@@ -51,15 +58,17 @@ export default function Home(){
                         </div>
                     </div>
                 </section>
-                <section className="w-full my-10 h-auto flex flex-col gap-2 items-center">
-                    <h2 className="md:max-w-2xl max-w-lg text-destaque leading-4.5 sm:leading-5 md:leading-8 font-extrabold text-azul-base text-center">PARA QUEM É A TERAPIA OCUPACIONAL E REABILITAÇÃO NEUROCOGNITIVA:</h2>
-                    <p className="md:max-w-xl max-w-lg whitespace-normal text-normal leading-[1.1] text-center"><strong>População Adulta e idosa que enfrentam dificuldades cognitivas e funcionais</strong> decorrentes de condições neurológicas como Doença de Alzheimer, outras demências, Acidente Vascular Encefálico (AVE) e lesões cerebrais</p>
+                <section id="nossosPacientes" className="w-full my-10 h-auto flex flex-col gap-2 items-center">
+                    <div className="mx-5">
+                        <h2 className="pb-2 md:max-w-2xl max-w-lg text-destaque leading-[1] font-extrabold text-azul-base text-left md:text-center text-balance"><strong className="font-extrabold md:text-destaque text-[2.6rem]">PARA QUEM É</strong> A TERAPIA OCUPACIONAL E REABILITAÇÃO NEUROCOGNITIVA:</h2>
+                        <p className="md:max-w-xl max-w-lg whitespace-normal text-normal leading-[1.1] md:text-center text-balance"><strong>População Adulta e idosa que enfrentam dificuldades cognitivas e funcionais</strong> decorrentes de condições neurológicas como Doença de Alzheimer, outras demências, Acidente Vascular Encefálico (AVE) e lesões cerebrais</p>
+                    </div>
                     <Carrossel></Carrossel>
                 </section>
-                <section className="w-full ">
+                <section id="nossosServicos" className="w-full ">
                     <div className=" h-[70%] flex flex-col items-center bg-degrade-cinza">
                         <img className="w-full max-w-[390px] h-[140px] my-4 self-center block bg-no-repeat bg-center bg-contain sm:bg-auto bg-[url(/images/nossosServicos.png)]"></img>
-                        <div className="">
+                        <div>
                             <Servicos></Servicos>
                         </div>
                     </div>
@@ -69,19 +78,19 @@ export default function Home(){
                     <div className="relative w-full h-[140px] content-center self-center block bg-[url('/images/agendamento.webp')] bg-no-repeat bg-center bg-cover sm:bg-auto">
                         <div className="absolute inset-0 bg-[#806438] opacity-40 z-0"></div>
                         <div className="w-full text-white md:max-w-7/10 lg:max-w-6/10 xl:max-w-4/10 relative flex flex-col gap-3 items-center text-center justify-self-center z-10">
-                            <p className="text-normal mx-8 xs:mx-25 font-bold leading-[1.2] tracking-wider">AGENDE UMA AVALIAÇÃO E DESCUBRA COMO A TERAPIA OCUPACIONAL PODE TRANSFORMAR SUA VIDA!</p>
+                            <p className="text-normal mx-6 xs:mx-25 font-bold leading-[1.2] tracking-wider text-center">AGENDE UMA AVALIAÇÃO E DESCUBRA COMO A TERAPIA OCUPACIONAL PODE TRANSFORMAR SUA VIDA!</p>
                             <Button className="bg-botao-wpp hover:bg-[#099656] text-white text-xs">AGENDAR AVALIAÇÃO</Button>
                         </div>                        
                     </div>
                 </section>
-                <section className="w-full h-[500px] mt-20">
+                <section id="comoFunciona" className="w-full h-auto md:mt-20">
                     <div className="flex flex-col items-center gap-5">
-                        <p className="max-w-[400px] text-destaque font-extrabold leading-4.5 sm:leading-5 md:leading-8 text-center text-azul-base">COMO FUNCIONA NOSSO ATENDIMENTO</p>
+                        <p className="max-w-[400px] text-destaque font-extrabold leading-4.5 sm:leading-5 md:leading-8 text-center text-azul-base text-balance">COMO FUNCIONA NOSSO ATENDIMENTO</p>
                         <NossoAtendimento></NossoAtendimento>
                         <Button className="mt-5 bg-dourado-base text-white hover:bg-[#bb8e47]">QUERO SABER MAIS</Button>
                     </div>
                 </section>
-                <section className="w-full">
+                <section id="quemSomos" className="w-full">
                     <QuemSomos></QuemSomos>
                 </section>
                 <section>
@@ -91,7 +100,9 @@ export default function Home(){
                     <NossoEspaco></NossoEspaco>
                 </section>
             </div>
-            <Footer></Footer>
+            <div id="localizacao">
+               <Footer></Footer>  
+            </div>
         </div>
     )
 }
